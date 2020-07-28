7ce724f012a0e94823219210c89267d8a2d7da30
Tammo Brüggemann <tammo.brueggemann97@gmail.com>
2020-07-28 12:26:43 +0200

    updated Serie5 with new comments

--- Serie5/mod_precision.f90
+++ Serie5/mod_precision.f90
@@ -1,5 +1,5 @@
 module mod_precision
-	integer, parameter :: sp = 4  ! single precision Ergebnis = 8.01512E-02
-	integer, parameter :: dp = 8  ! double precision Ergebnis = 0.132499
+	integer, parameter :: sp = 4  ! single precision Ergebnis = 30.14
+	integer, parameter :: dp = 8  ! double precision Ergebnis = 53.99
 	integer, parameter :: wp = sp ! working precision
 end module
--- Serie5/predator_prey.f90
+++ Serie5/predator_prey.f90
@@ -4,18 +4,20 @@ program main
  
 
     
-    real(kind=wp), allocatable :: D(:,:), x0(:), y0(:), DiffMat(:,:)
-    real(kind=wp) :: alpha,beta,lambda,delta,gamma,mu, T, dt, kappa, h, t0, t1, t2
-    integer :: i,j,steps, N
+    real(kind=wp), allocatable :: DiffMat(:,:), values_pred(:), values_prey(:)
+    real(kind=wp) :: alpha, beta, lambda, delta, gamma, mu, T, dt, kappa, h, t0, t1, t2
+    integer :: i, j, steps, N
 
-    real(kind=wp), allocatable :: values_pred(:), values_prey(:)
 
+    !namelist Gruppen definieren
     namelist /model_parameters/ alpha, beta, gamma, delta, lambda, mu
     namelist /spatial_parameters/ kappa, N 
     namelist /time_parameters/ t0, T, dt
 
+    !namelist Datei öffnen
     open(unit = 12, file = 'predatorprey.nml', action = 'read')
 
+    !einzelne Gruppen auslesen
     read(unit = 12,nml = model_parameters)
     read(unit = 12,nml = spatial_parameters)
     read(unit = 12,nml = time_parameters)
@@ -24,25 +26,24 @@ program main
 
     steps = (T - t0) / dt  ! = n
 
+    !falls die Azahl der Schritte die gegebene Bedingung in Abhängigkeit von
+    !Anzahl der Boxen, Diffusionskonstante und Endzeitpunkt nicht erfüllt, wird diese
+    !manuell so gesetzt
     if(steps < int(2*N*N*kappa*T)) then
-        steps = int(2*N*N*kappa*T)
+        steps = int(4*N*N*kappa*T)
+        dt = (T-t0) / steps !Wenn Steps angepasst werden, müssen wir auch deltaT anpassen, da wir sonst über T hinaus laufen
     endif
 
-    h = 1 / dble(N)
+    h = 1 / dble(N) !Da wir das Intervall von 0 bis 1 Räumlich partitionieren
 
-    !Diffusions Matrix
+    !Speicher allozieren
     allocate(DiffMat(N,N))
-    allocate(D(N,N))
-
-    allocate(x0(N))
-    allocate(y0(N))
-
     allocate(values_prey(N))
     allocate(values_pred(N))
 
-
-    do i = 1, N
-        do j = 1, N
+    !Diffusionsmatrix erstellen
+    do j = 1, N
+        do i = 1, N
             if((i == j) .and. (i == 1 .or. i == N)) then
                 DiffMat(i,j) = -1
             else if (i == j) then
@@ -55,29 +56,26 @@ program main
         enddo
     enddo
 
-    D = (kappa / (h * h))  * DiffMat
+    DiffMat = (kappa / (h * h))  * DiffMat !skalierte Diffusionsmatrix
 
 
-    call random_number(x0)
-    call random_number(y0)
-    values_prey = x0
-    values_pred = y0
+    call random_number(values_prey) !random_number füllt das Array mit Zufälligen Zahlen zwischen 0 und 1
+    call random_number(values_pred)
 
 
-    
+    !timeloop
     call cpu_time(t1)
-    do i = 2, steps
+    do i = 2, steps + 1
         call euler_at_one_point(values_prey, values_pred, dt)
-  
-
+        write(*,*) i
     enddo
     call cpu_time(t2)
 
-   write(*,*) t2-t1 
+    write(*,*) t2-t1 
 
     open(unit=12, file='values_prey.txt', status='replace', action='write')
-    write(12, '(E20.6)') values_prey
-    close(unit=12)
+    write(12, '(E20.6)') values_prey !E steht für Real Numbers(Exponent Notation), 
+    close(unit=12)                   !die 20 für die Feldbreite und die 6 für die Anzahl der Nachkommastellen (fractional Part)
 
     open(unit=13, file='values_pred.txt', status='replace', action='write')
     write(13, '(E20.6)') values_pred
@@ -85,28 +83,25 @@ program main
     
     deallocate(values_prey)
     deallocate(values_pred)
-    deallocate(D)
     deallocate(DiffMat)
-    deallocate(x0)
-    deallocate(y0)
 
 
     
     contains
-    subroutine euler_at_one_point(values_prey, values_pred,deltaT)
-        real(kind=wp), intent(INOUT) :: values_prey(:), values_pred(:) !Beidseitiger Informationsfluss, Aufrufende Programm Einheit <->  Funktion
-        real(kind=wp), intent(IN) :: deltaT!Wert wird in der Funktion nicht verändert und es findet kein Rückfluss
-        !der Informationen in die aufrundende Programm Einheit statt
-        real(kind=wp), allocatable :: temp_array(:)
-        allocate(temp_array(N))
+    subroutine euler_at_one_point(values_prey, values_pred, deltaT)
+        real(kind=wp), intent(INOUT) :: values_prey(:), values_pred(:) !Beidseitiger Informationsfluss, Aufrufende Programmeinheit <->  Funktion
+        real(kind=wp), intent(IN) :: deltaT  !Wert wird in der Funktion nicht verändert und es findet kein Rückfluss
+                                             !der Informationen in die aufrufende Programmeinheit statt
+        real(kind=wp), allocatable :: temp_array(:)  !Temp_array benutzen wir, damit die Änderung beider Bestände gleichzeitig in Kraft treten
+                                                     !und sich nicht gegenseitig beeinflussen
+        allocate(temp_array(steps))
+        temp_array = values_prey 
 
-        temp_array = values_prey
+        values_prey = values_prey + deltaT * (MATMUL(DiffMat, values_prey) + values_prey * (alpha - beta * values_pred &
+        & - lambda * values_prey))
 
-        values_prey = values_prey + deltaT * ((MATMUL(D, values_prey) + values_prey * alpha - values_prey * beta * &
-        & values_pred - values_prey * lambda * values_prey))
-
-        values_pred = values_pred + deltaT * ((MATMUL(D, values_pred) + values_pred * delta *  temp_array - values_pred * gamma - &
-        & values_pred * mu * values_pred))
+        values_pred = values_pred + deltaT * (MATMUL(DiffMat, values_pred) + values_pred * (delta * temp_array - gamma &
+        & - mu * values_pred))
 
         deallocate(temp_array)
     end subroutine
